ba531027426cc9d4f434248556449a65b1a813a4
DOZCAM <huck-cameron@hotmail.fr>
2022-03-08 16:47:42 +0100

    p5 debut

---
+++ back/script.js
@@ -0,0 +1,18 @@
+fetch('http://localhost:3000/api/products')
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+    console.log("erreur")
+    // Une erreur est survenue
+  });
+const products =
+
+for (let product of products) {
+   console.log("");
+}
