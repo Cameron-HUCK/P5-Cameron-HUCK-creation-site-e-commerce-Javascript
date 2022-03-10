70d6448ec0acfe70a3f9791ab8fdfeecec38ae6e
DOZCAM <huck-cameron@hotmail.fr>
2022-03-10 16:58:09 +0100

    UrlSearch

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,4 @@
+/* Affichage des cartes des produits KANAP sur la page index.html */
 fetch('http://localhost:3000/api/products')
   .then(function(res) {
     if (res.ok) {
@@ -11,15 +12,44 @@ let products = value;
 
   for (let i = 0; i < products.length; i++) {
 
-    const productsList = document.getElementById('item');
+/*L'ID parent*/
+    const productsList = document.getElementById('items');
 
-    let productsName = document.createElement('h3');
+/* Lien produit qui conduit vers la page produit / Parent de la balise Article*/
+  let productsLink = document.createElement("a");
+  productsLink.setAttribute("href", './product.html?id=${products-ID}');
+  productsList.appendChild(productsLink);
+  console.log(productsLink)
+
+/* Creation de la Balise HTML Article / Parent de (img, h3 , p) */
+  let productArticle = document.createElement("article");
+  productsLink.appendChild(productArticle);
+
+/* Image des differents produits */
+  let productsImg = document.createElement("img");
+  productsImg.setAttribute("src", products[i].imageUrl);
+  productsImg.setAttribute("alt", products[i].altTxt);
+  productArticle.appendChild(productsImg);
+
+/* Creation des titres des produits (h3) avec comme classe "productName" */
+
+    let productsName = document.createElement("h3");
     productsName.classList.add("productName");
     productsName.textContent = products[i].name;
-    console.log(productsName)
-}
+    productArticle.appendChild(productsName);
 
-  })
+/* Creation du texte, description des produits */
+    let productsDescription = document.createElement("p");
+    productsDescription.classList.add("productDescription");
+    productsDescription.textContent = products[i].description;
+    productArticle.appendChild(productsDescription);
+
+    var str = "file:///Users/huckcameron/Documents/P5-HUCK-Cameron/front/html/index.html";
+    var url = new URL(str);
+    var name = url.searchParams.get("name");
+    console.log(name);
+  } 
+})
   .catch(function(err) {
     console.log("erreur");
     console.log(err);
