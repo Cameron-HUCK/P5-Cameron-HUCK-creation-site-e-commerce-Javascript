0d498a473b4e85103c2fd0e6d6d01b82c21c4c92
DOZCAM <huck-cameron@hotmail.fr>
2022-03-14 18:49:19 +0100

    UrlSearchStream

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,27 @@
+/* Recuperation de l"id de chaque produit par page*/
 let urlString =  document.location.href;
 let urlObject =  new URL('file:///Users/huckcameron/Documents/P5-HUCK-Cameron/front/html/index.html');
 console.log(urlString);
 
 var paramsString = ".searchParams&id=${products[i]._id}";
-var searchParams = new URLSearchParams (urlString);
+var searchParams = new URLSearchParams (urlObject);
+console.log(searchParams)
 
 const id = searchParams.get("id");
-console.log(leId)
+console.log(id);
 
+/* Appel de L'api */
+fetch('http://localhost:3000/api/products/')
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+
+let products = value;
+
+  for (let i = 0; i < products.length; i++) {
+  } 
+})
