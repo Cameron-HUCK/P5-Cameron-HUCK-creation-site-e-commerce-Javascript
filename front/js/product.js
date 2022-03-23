fdda3f36b14680c275153c2bc82b9131fa5f7a29
DOZCAM <huck-cameron@hotmail.fr>
2022-03-23 11:52:47 +0100

    Partie 7 finis

--- front/js/product.js
+++ front/js/product.js
@@ -36,43 +36,43 @@ contentDescription_Product.textContent = product.description;
 
 // Modification des couleurs proposer de chaque produit en recuperant chaque couleur dans le tableau
 let colorsProduct = product.colors;
-let colors_Id = document.getElementById('colors');
+let colors_Select = document.getElementById('colors');
 colorsProduct.forEach(function (element, key) {
-  colors_Id[key] = new Option(element,);
+  colors_Select[key] = new Option(element);
 });
 // Au click sur le bouton "ajout panier ", je veux stocker dans un tableau, dans le localStorage, les informations du produit : _id, qty, color
   
 //localStorage
 
+//Permet de voir ce que contient 'cart'
+console.log(JSON.parse(localStorage.getItem("cart")));
+
 //Ecouter le bouton et empecher reactualisation de lampage au click
 let idCart = document.getElementById('addToCart');
 idCart.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log(idCart);
-})
+
 //Recuperation des informations sur la commande du produit.
 let qtyProduct = document.getElementById('quantity');
 
-let productToAdd = [];
+let productToAdd = {};
   productToAdd._id = productId;
-  productToAdd.qty = qtyProduct;
-  productToAdd.color = colors_Id;
+  productToAdd.qty = qtyProduct.value;
+  productToAdd.color = colors_Select.value;
 
-  let cart = [];
+  let cart = JSON.parse(localStorage.getItem("cart"));
+  if (cart == null){
+    cart = [];
+  } 
+// Si le produit est deja present (meme id, meme couleur), ajoute +1 produit dans le panier.
+  else (productId && [colors_Select.value]); {
+    qtyProduct.value++;
+  }
+
   cart.push(productToAdd);
-  localStorage.setItem("cart", cart);
-
-  let confirmWindow = () => {
-    if (idCart.confirm(productToAdd))
-    window.location.href = "../html/cart.html"
-  else{
-    window.location.href = "../html/index.html";
-  }
-  }
-
-  console.log(confirmWindow);
+  localStorage.setItem("cart", JSON.stringify(cart));
   console.log(cart);
-
+  })
 })
 .catch(function(err) {
     console.log("erreur");
