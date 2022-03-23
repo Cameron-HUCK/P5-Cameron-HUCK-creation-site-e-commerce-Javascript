ae40e1d0a5cb59d62c2e4fcfd713bfd48544fa25
DOZCAM <huck-cameron@hotmail.fr>
2022-03-23 19:18:27 +0100

    Partie 7 finis

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,28 +1,33 @@
-// Recuperation des elements dans le localStorage
+// Recuperation des elements du panier
+let cartStorage = JSON.parse(localStorage.getItem("cart"));
 
-let panierlocalStorage = JSON.parse(localStorage.getItem("Selection"));
-if (panierlocalStorage  == null){
-  panierlocalStorage  = [];
+// Si le panier est vide
+if (cartStorage == null){
+
+  // Il faut creer un tableau qui recoit les produits
+  cartStorage = [];
+//envoie l'objet dans le tableau
+  cartStorage.push(cart);
+  localStorage.setItem("cart", JSON.stringify(cartStorage));
 } 
-console.log(panierlocalStorage);
-console.log(typeof(panierlocalStorage ));
-console.log(JSON.parse(localStorage.getItem("cart")));
+console.log(panierProduct);
+console.log(typeof(panierProduct));
 
 //Traitement des cas possibles de PanierProduit et remplissages de <section id="cart__items">
 
-if (panierProduct == null || panierProduct == 0) {
+/*if (panierProduct == null || panierProduct == 0) {
   let messagePanierVide = document.getElementById("cart__items");
   messagePanierVide.insertAdjacentHTML('afterend',
   '<div class="cart__item__img">',
       '<p> Votre panier ne comporte actuellement aucun produit </p>',
   '</div>'
   );
+  console.log(messagePanierVide)
 }else{
   // Si le panier n'est pas vide 
   for(i=0; i < panierProduct.lenght; i++);
 
 let insertProduct = document.getElementById('cart__idems');
-console.log(insertProduct);
 insertProduct.insertAdjacentHTML("afterend",
 `<article class="cart__item" data-id="${panierProduct[i]._id}" data-color="${panierProduct[i].colors}">,
                 '<div class="cart__item__img">',
@@ -30,14 +35,14 @@ insertProduct.insertAdjacentHTML("afterend",
                 </div>,
                 <div class="cart__item__content">,
                   <div class="cart__item__content__description">,
-                    <h2>${panierProduct.name}</h2>,
-                    <p>${panierProduct[i].colors}</p>,
-                    <p>${panierProduct[i].price}</p>,
+                    <h2>Nom du produit</h2>,
+                    <p>Vert</p>,
+                    <p>42,00 €</p>,
                   </div>,
                   <div class="cart__item__content__settings">,
                     <div class="cart__item__content__settings__quantity">,
                       <p>Qté : </p>,
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${panierProduct[i].qty}">,
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">,
                     </div>,
                     <div class="cart__item__content__settings__delete">,
                       <p class="deleteItem">Supprimer</p>,
@@ -46,4 +51,4 @@ insertProduct.insertAdjacentHTML("afterend",
                 </div>,
               </article>`
               );
-}
+}*/
